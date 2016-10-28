72ae286176ebd84de09e1533079bd7efa5606e7d
陈昭燊 <alex_coolboy@163.com>
2016-10-28 19:58:34 +0800

    修改目录结构

--- tomCatTV/tomCatTV/TVClasses/others/AppDelegate.swift
+++ tomCatTV/tomCatTV/TVClasses/others/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow()
+        let homeVC = HomeViewController()
+        homeVC.view.backgroundColor = UIColor.orange
+        self.window?.rootViewController = homeVC
+        self.window?.makeKeyAndVisible()
         return true
     }
 
